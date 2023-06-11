81f24877bc1ed2b4ef2bfc90f61aa15ccab84b0e
Maksim Pegov <maksim.peg@gmail.com>
2023-06-11 21:27:25 +0200

    Jwt and Spring Security connect

---
+++ todos/src/main/java/com/maksimpegov/todos/config/JwtAuthFilter.java
@@ -0,0 +1,36 @@
+package com.maksimpegov.todos.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userId;
+        if (authHeader != null || !authHeader.startsWith("Bearer ")) {
+            jwt = authHeader.substring(7);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userId = jwtService.getUserId(jwt);
+    }
+}
---
+++ todos/src/main/java/com/maksimpegov/todos/config/JwtService.java
@@ -0,0 +1,8 @@
+package com.maksimpegov.todos.config;
+
+public class JwtService {
+
+    public String getUserId(String jwt) {
+        return null;
+    }
+}
