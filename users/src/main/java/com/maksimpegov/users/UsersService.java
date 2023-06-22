1200dd491e6940dda2591e4c8ce928bcb416a8d2
Maksim Pegov <maksim.peg@gmail.com>
2023-06-22 18:33:58 +0200

    [#8] Implement registration functional

---
+++ users/src/main/java/com/maksimpegov/users/UsersService.java
@@ -0,0 +1,46 @@
+package com.maksimpegov.users;
+
+import com.maksimpegov.users.models.User;
+import com.maksimpegov.users.models.UserServiceResponse;
+import com.maksimpegov.users.models.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Date;
+
+@Service
+public class UsersService {
+
+    private final RestTemplate restTemplate;
+    private UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(RestTemplate restTemplate, UsersRepository usersRepository) {
+        this.restTemplate = restTemplate;
+        this.usersRepository = usersRepository;
+    }
+
+    public UserServiceResponse registerUser(User user) {
+        try {
+            if (user.getUsername() == null || user.getPassword() == null) {
+                return new UserServiceResponse("400", "Your request is not valid");
+            }else if (!user.passwordValidation() || !user.usernameValidation()) {
+                return new UserServiceResponse("400", "Username or password is not valid. Username: min 3 symbols, password: min 6 symbols)");
+            }else if (usersRepository.findByUsername(user.getUsername()) != null) {
+                return new UserServiceResponse("409", "User with this username already exists");
+            }
+            user.setCreated_at(new Date());
+            usersRepository.save(user);
+            return new UserServiceResponse("201", "User created successfully");
+        } catch (Exception e) {
+            return new UserServiceResponse("500", "Error. " + e.getMessage());
+        }
+    }
+
+    public String deleteUser(String userId) {
+        restTemplate.delete("http://todos/todos/clear/" + userId);
+        String response = "User " + userId + " was deleted";
+        return response;
+    }
+}
--- users/src/main/java/com/maksimpegov/users/models/User.java
+++ users/src/main/java/com/maksimpegov/users/models/User.java
@@ -1,35 +1,53 @@
 package com.maksimpegov.users.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
 import java.util.UUID;
 
+@Setter
+@Getter
+@Entity(name = "User")
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(name = "user_id_unique", columnNames = "id"),
+        @UniqueConstraint(name = "user_username_unique", columnNames = "username")
+})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) // to avoid hibernate extra properties
 public class User {
+    @Id
+    @Column(name = "id", updatable = false)
     private String id;
+
+    @Column(name = "username", nullable = false)
     private String username;
+
+    @Column(name = "password", nullable = false)
     private String password;
 
+    @Column(name = "created_at", nullable = false)
+    private Date created_at;
+
+    public Boolean passwordValidation(){
+        setPassword(password.trim());
+        return this.password.length() >= 6;
+    }
+
+    public Boolean usernameValidation(){
+        setUsername(username.trim());
+        return this.username.length() >= 3;
+    }
+
     public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+        this.username = username.trim();
+        this.password = password.trim();
         this.id = UUID.randomUUID().toString();
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public User() {
+        this.id = UUID.randomUUID().toString();
     }
 }
