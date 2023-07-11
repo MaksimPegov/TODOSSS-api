6e2316085fbeb05745b0013dbc6150af26b7401e
Maksim Pegov <maksim.peg@gmail.com>
2023-07-11 22:26:04 +0200

    Endpoints annotation for todo controller

--- todos/src/main/java/com/maksimpegov/todos/TodosController.java
+++ todos/src/main/java/com/maksimpegov/todos/TodosController.java
@@ -4,6 +4,7 @@ import com.maksimpegov.todos.models.TodoServiceResponse;
 import com.maksimpegov.todos.todo.Todo;
 import com.maksimpegov.todos.todo.TodoDto;
 import com.maksimpegov.todos.todo.TodoMapper;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,13 +21,14 @@ public class TodosController {
         this.mapper = TodoMapper.INSTANCE;
     }
 
-
+    @ApiOperation(value = "Get all todos for a user", notes = "Provide userId in path", response = TodoDto.class, responseContainer = "List")
     @GetMapping(path = "{userId}")
     public ResponseEntity<Object> getTodos(@PathVariable Long userId) {
         TodoServiceResponse response = todosService.getTodos(userId);
         return ResponseBuilder.build(response);
     }
 
+    @ApiOperation(value = "Add a todo for a user", notes = "Provide userId in path and text of todo in body", response = TodoDto.class)
     @PostMapping(path = "{userId}")
     public ResponseEntity<Object> addTodos(@RequestBody TodoDto todoDto, @PathVariable Long userId) {
         Todo todo = mapper.map(todoDto);
@@ -34,19 +36,22 @@ public class TodosController {
         return ResponseBuilder.build(response);
     }
 
-    @PatchMapping()
+    @ApiOperation(value = "Edit a todo", notes = "Provide only todoId and a todo field that you want to edit in body", response = TodoDto.class)
+    @PatchMapping
     public ResponseEntity<Object> editTodos(@RequestBody TodoDto newTodoPartDto) {
         Todo newTodoPart = mapper.map(newTodoPartDto);
         TodoServiceResponse response = todosService.editTodo(newTodoPart);
         return ResponseBuilder.build(response);
     }
 
+    @ApiOperation(value = "Delete a todo", notes = "Provide todoId in path", response = TodoDto.class)
     @DeleteMapping(path = "/{userId}/{todoId}") // userId may be removed in the future
     public ResponseEntity<Object> deleteTodos(@PathVariable String userId, @PathVariable String todoId) {
         TodoServiceResponse response = todosService.deleteTodo(todoId);
         return ResponseBuilder.build(response);
     }
 
+    @ApiOperation(value = "Delete all todos for a user", notes = "Provide userId in path", response = TodoDto.class)
     @DeleteMapping(path = "/clear/{userId}")
     public ResponseEntity<Object> clearTodos(@PathVariable Long userId) {
         TodoServiceResponse response = todosService.clearTodos(userId);
